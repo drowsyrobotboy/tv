52e6d876a07107cd7e036d60b25dfb6e6ecd84d6
Maruthi Sharma <maruthisharma@live.in>
2014-01-30 23:30:36 +0530

    added ng-app directive to index page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app>
 <head>
     <title>Mark Three Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
